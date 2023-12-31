e978724de0fe6bcd655f0f80462760122deffd07
Henry Loughlin <64449842+hloughlin9@users.noreply.github.com>
2023-12-31 18:49:28 -0500

    Update setup.py
    Added folium dependency.

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='strava-parser',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     install_requires=[
         'gpxpy',
         'pandas',
         'numpy',
         'geopy',
+        'matplotlib',
+        'folium',
     ],
 )
